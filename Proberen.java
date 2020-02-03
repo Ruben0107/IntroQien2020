52b974249d248fa1bf7935ef6d95f076e4a93e46
Ruben van Rij <Ruben@MacBook-Pro-van-Ruben-3.local>
2020-02-03 13:47:03 +0100

    go2

--- Proberen.java
+++ Proberen.java
@@ -1,3 +1,10 @@
-Hallo
-Nog een lijn
-derde regel
+class Hallo{
+	public static void main(String[] args){
+		System.out.println("Hallo");
+		voordoen();
+		voordoen();
+	}
+	static void voordoen(){
+		System.out.println("hoi");
+	}
+}
